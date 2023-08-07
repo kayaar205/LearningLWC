23b7d703138fee597d2bdaa44a75549d0c7dfb1f
Kesavan Rajasekaran <kesavan@caftius.com>
2023-08-07 23:19:43 +0530

    Features1

--- force-app/main/default/lwc/demoLWC/demoLWC.js
+++ force-app/main/default/lwc/demoLWC/demoLWC.js
@@ -1,5 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class DemoLWC extends LightningElement {
-
-}
+export default class DemoLWC extends LightningElement {}
---
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    name = 'LearningLWC';
+    company = 'Salesforce';
+    designation = 'Fresher';
+    salary = '10000';
+    status = 'Active'
+}
